5b3534862c70a332c623f67975d3f8f3e73f2b17
ahmadwaluyo <ahmad.umb15@gmail.com>
2020-05-30 07:35:06 +0700

    add starting point

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
 const router = require('./routes');
-const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 app.use('/', router);
 
 module.exports = app;
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on Port ${PORT}`)
-// })
